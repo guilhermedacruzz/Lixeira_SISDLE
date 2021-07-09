4a4bc60c12470e02111ebbbed5a22e122dc12fb7
indexGuilherme <gddcruz04@gmail.com>
2021-07-09 11:03:20 -0300

    garantindo que a distância morta do sensor será desconsiderada

--- src/utils/calculatePorc.h
+++ src/utils/calculatePorc.h
@@ -13,6 +13,10 @@ extern Settings settings;
 // Capacidade = (1.0 - (<leitura> - <distancia até o topo>) / (<distancia até o fundo> - <distancia até o topo>)) * 100
 int convertMM(int mm) {
     int DEAD_AREA = atoi(settings.distanceCover.c_str()) * 10;
+
+    if(DEAD_AREA < 5)
+        DEAD_AREA = 5;
+        
     int MAX = atoi(settings.distanceBottom.c_str()) * 10;
 
     int result = (int)((1.0 - (mm - DEAD_AREA) / (float)(MAX - DEAD_AREA)) * 100);
