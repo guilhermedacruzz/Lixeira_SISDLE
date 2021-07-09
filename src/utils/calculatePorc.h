5ca06e0fb05c6ec515146ab088d13c8b1a19b20a
indexGuilherme <gddcruz04@gmail.com>
2021-07-09 10:41:18 -0300

    arrumando bug que retornava porcentagem da capacidade maior que 100

--- src/utils/calculatePorc.h
+++ src/utils/calculatePorc.h
@@ -15,7 +15,14 @@ int convertMM(int mm) {
     int DEAD_AREA = atoi(settings.distanceCover.c_str()) * 10;
     int MAX = atoi(settings.distanceBottom.c_str()) * 10;
 
-    return (int)((1.0 - (mm - DEAD_AREA) / (float)(MAX - DEAD_AREA)) * 100);
+    int result = (int)((1.0 - (mm - DEAD_AREA) / (float)(MAX - DEAD_AREA)) * 100);
+
+    if(result < 0)
+        result = 0;
+    else if(result > 100)
+        result = 100;
+        
+    return result;
 }
 
 #endif
